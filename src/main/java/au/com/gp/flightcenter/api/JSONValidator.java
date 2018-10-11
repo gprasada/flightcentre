12ef02dfcd2b9efb90559482cf60eaddf68bd3ac
Guru Prasad <gprasad@192-168-1-6.tpgi.com.au>
2018-10-11 18:19:20 +1100

    adding floe reference to createtrip subflow

--- src/main/java/au/com/gp/flightcenter/api/JSONValidator.java
+++ src/main/java/au/com/gp/flightcenter/api/JSONValidator.java
@@ -2,8 +2,8 @@ package au.com.gp.flightcenter.api;
 
 public class JSONValidator {
 	
-	public void validate(String payload) {
-		System.out.println(" payload is "+payload);
+	public void validate(String arg0) throws ValidationException{
+		System.out.println(" payload is "+arg0);
 	}
 
 }
---
+++ src/main/java/au/com/gp/flightcenter/api/ValidationException.java
@@ -0,0 +1,9 @@
+package au.com.gp.flightcenter.api;
+
+public class ValidationException extends Exception {
+	
+	public ValidationException(String msg) {
+		super(msg);
+	}
+
+}
